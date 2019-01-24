db116320f67c40d6d34b3a4adaa16031ff5a366e
SS2U <46984353+SS2U@users.noreply.github.com>
2019-01-24 15:03:30 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -71,8 +71,8 @@
 			$keyword = $_GET["keyword"];
 			if(isset($keyword))
 			{
-				echo "ttttttttttt";
-				$sql_search ="SELECT * FROM tbl_tr-load WHERE pea LIKE '%".$keyword."%'";
+			
+				$sql_search ="SELECT * FROM tbl_tr WHERE pea LIKE '%".$keyword."%'";
 				$query_search = mysqli_query($conn,$sql_search);
 			}
 		?>
