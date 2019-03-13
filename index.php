c589a0a56f43ef26cbc6b7a3922023dea6192942
SS2U <46984353+SS2U@users.noreply.github.com>
2019-03-13 23:01:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -93,7 +93,7 @@
 						</div>
 						<button type="submit" class="btn btn-primary btn-block">ค้นหา</button>
 						<p>&nbsp;</p>
-                        			<button type="button" href="addload.html" class="btn btn-primary btn-block">เพิ่มข้อมูลหม้อแปลง</button>
+                        			<a href="addload.html" class="btn btn-primary btn-block">เพิ่มข้อมูลหม้อแปลง</a>
 					</form>
 				</div>
 			</div>
