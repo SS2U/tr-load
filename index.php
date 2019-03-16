a7ea9ac61e324388a0d7055d89335e8c2a733d4c
seksun <37170911+Seksun2you@users.noreply.github.com>
2019-03-16 22:52:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -93,7 +93,9 @@
 						</div>
 						<button type="submit" class="btn btn-primary btn-block">ค้นหา</button>
 						<p>&nbsp;</p>
-                        			<a href="addload.html" class="btn btn-primary btn-block">เพิ่มข้อมูลหม้อแปลง</a>
+                        <a href="addload.html" class="btn btn-primary btn-block">แก้ไขข้อมูลหม้อแปลง</a>
+						<p>&nbsp;</p>
+                        <a href="addload.html" class="btn btn-primary btn-block">เพิ่มข้อมูลหม้อแปลง</a>
 					</form>
 				</div>
 			</div>
