001411d4a810a31e32376c25ca49b693aa21cb5f
seksun <37170911+Seksun2you@users.noreply.github.com>
2019-03-19 20:55:37 +0700

    เพิ่มตัวแปร

--- control.js
+++ control.js
@@ -1,6 +1,23 @@
 function insert()
 {
 	var pea = document.getElementById("pea").value;
+	var pea = document.getElementById("location").value;
+	var pea = document.getElementById("kva").value;
+	var pea = document.getElementById("phase").value;
+	var pea = document.getElementById("C_A_L").value;
+	var pea = document.getElementById("C_B_L").value;
+	var pea = document.getElementById("C_C_L").value;
+	var pea = document.getElementById("C_A_R").value;
+	var pea = document.getElementById("C_B_R").value;
+	var pea = document.getElementById("C_C_R").value;
+	var pea = document.getElementById("V_AN_L").value;
+	var pea = document.getElementById("V_BN_L").value;
+	var pea = document.getElementById("V_CN_L").value;
+	var pea = document.getElementById("V_AN_R").value;
+	var pea = document.getElementById("V_BN_R").value;
+	var pea = document.getElementById("V_CN_R").value;
+	
+	
 	var formData = new FormData();
 	formData.append('pea',pea);
 	$.ajax({
--- insert.php
+++ insert.php
@@ -1,8 +1,42 @@
 <?php
 	require('connect-db.php'); //เชื่อมต่อ database
 	$pea = $_POST["pea"];
+	$pea = $_POST["location"];
+	$pea = $_POST["kva"];
+	$pea = $_POST["phase"];
+	$pea = $_POST["C_A_L"];
+	$pea = $_POST["C_B_L"];
+	$pea = $_POST["C_C_L"];
+	$pea = $_POST["C_A_R"];
+	$pea = $_POST["C_B_R"];
+	$pea = $_POST["C_C_R"];
+	$pea = $_POST["V_AN_L"];
+	$pea = $_POST["V_BN_L"];
+	$pea = $_POST["V_CN_L"];
+	$pea = $_POST["V_AN_R"];
+	$pea = $_POST["V_BN_R"];
+	$pea = $_POST["V_CN_R"];
+	
+	
 	
 	$sql_text_insert = "INSERT INTO tbl_tr(pea) VALUES('$pea')";
+	$sql_text_insert = "INSERT INTO tbl_tr(location) VALUES('location')";
+	$sql_text_insert = "INSERT INTO tbl_tr(kva) VALUES('$kva')";
+	$sql_text_insert = "INSERT INTO tbl_tr(phase) VALUES('$phase')";
+	$sql_text_insert = "INSERT INTO tbl_tr(current-a-l) VALUES('$C_A_L')";
+	$sql_text_insert = "INSERT INTO tbl_tr(current-b-l) VALUES('$C_B_L')";
+	$sql_text_insert = "INSERT INTO tbl_tr(current-c-l) VALUES('C_C_L')";
+	$sql_text_insert = "INSERT INTO tbl_tr(current-a-r) VALUES('$C_A_R')";
+	$sql_text_insert = "INSERT INTO tbl_tr(current-b-r) VALUES('$C_B_R')";
+	$sql_text_insert = "INSERT INTO tbl_tr(current-c-r) VALUES('$C_C_R')";
+	$sql_text_insert = "INSERT INTO tbl_tr(voltage-an-l) VALUES('$V_AN_L')";
+	$sql_text_insert = "INSERT INTO tbl_tr(voltage-bn-l) VALUES('$V_BN_L')";
+	$sql_text_insert = "INSERT INTO tbl_tr(voltage-cn-l) VALUES('$V_CN_L')";
+	$sql_text_insert = "INSERT INTO tbl_tr(voltage-an-r) VALUES('$V_AN_R')";
+	$sql_text_insert = "INSERT INTO tbl_tr(voltage-bn-r) VALUES('$V_BN_R')";
+	$sql_text_insert = "INSERT INTO tbl_tr(voltage-cn-r) VALUES('$V_CN_R')";
+	
+	
 	mysqli_query($conn,$sql_text_insert);
 	echo "Insert Complete.....";
 ?>
