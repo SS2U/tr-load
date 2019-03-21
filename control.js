c33e4c55406ca23088863bb373e91128cdb9eb6f
SS2U <46984353+SS2U@users.noreply.github.com>
2019-03-21 13:44:38 +0700

    Add files via upload

--- control.js
+++ control.js
@@ -1,25 +1,43 @@
 function insert()
 {
 	var pea = document.getElementById("pea").value;
-	var pea = document.getElementById("location").value;
-	var pea = document.getElementById("kva").value;
-	var pea = document.getElementById("phase").value;
-	var pea = document.getElementById("C_A_L").value;
-	var pea = document.getElementById("C_B_L").value;
-	var pea = document.getElementById("C_C_L").value;
-	var pea = document.getElementById("C_A_R").value;
-	var pea = document.getElementById("C_B_R").value;
-	var pea = document.getElementById("C_C_R").value;
-	var pea = document.getElementById("V_AN_L").value;
-	var pea = document.getElementById("V_BN_L").value;
-	var pea = document.getElementById("V_CN_L").value;
-	var pea = document.getElementById("V_AN_R").value;
-	var pea = document.getElementById("V_BN_R").value;
-	var pea = document.getElementById("V_CN_R").value;
+	var loc = document.getElementById("location").value;
+	var kva = document.getElementById("kva").value;
+	var phase = document.getElementById("phase").value;
+	var C_A_L = document.getElementById("C_A_L").value;
+	var C_B_L = document.getElementById("C_B_L").value;
+	var C_C_L = document.getElementById("C_C_L").value;
+	var C_A_R = document.getElementById("C_A_R").value;
+	var C_B_R = document.getElementById("C_B_R").value;
+	var C_C_R = document.getElementById("C_C_R").value;
+	var V_AN_L = document.getElementById("V_AN_L").value;
+	var V_BN_L = document.getElementById("V_BN_L").value;
+	var V_CN_L = document.getElementById("V_CN_L").value;
+	var V_AN_R = document.getElementById("V_AN_R").value;
+	var V_BN_R = document.getElementById("V_BN_R").value;
+	var V_CN_R = document.getElementById("V_CN_R").value;
 	
 	
 	var formData = new FormData();
+	
 	formData.append('pea',pea);
+	formData.append('loc',loc);
+	formData.append('kva',kva);
+	formData.append('phase',phase);
+	formData.append('C_A_L',C_A_L);
+	formData.append('C_B_L',C_B_L);
+	formData.append('C_C_L',C_C_L);
+	formData.append('C_A_R',C_A_R);
+	formData.append('C_B_R',C_B_R);
+	formData.append('C_C_R',C_C_R);
+	formData.append('V_AN_L',V_AN_L);
+	formData.append('V_BN_L',V_BN_L);
+	formData.append('V_CN_L',V_CN_L);
+	formData.append('V_AN_R',V_AN_R);
+	formData.append('V_BN_R',V_BN_R);
+	formData.append('V_CN_R',V_CN_R);
+	
+	
 	$.ajax({
 			url:'insert.php',
 			method:'POST',
--- insert.php
+++ insert.php
@@ -1,42 +1,24 @@
 <?php
 	require('connect-db.php'); //เชื่อมต่อ database
 	$pea = $_POST["pea"];
-	$pea = $_POST["location"];
-	$pea = $_POST["kva"];
-	$pea = $_POST["phase"];
-	$pea = $_POST["C_A_L"];
-	$pea = $_POST["C_B_L"];
-	$pea = $_POST["C_C_L"];
-	$pea = $_POST["C_A_R"];
-	$pea = $_POST["C_B_R"];
-	$pea = $_POST["C_C_R"];
-	$pea = $_POST["V_AN_L"];
-	$pea = $_POST["V_BN_L"];
-	$pea = $_POST["V_CN_L"];
-	$pea = $_POST["V_AN_R"];
-	$pea = $_POST["V_BN_R"];
-	$pea = $_POST["V_CN_R"];
-	
-	
-	
-	$sql_text_insert = "INSERT INTO tbl_tr(pea) VALUES('$pea')";
-	$sql_text_insert = "INSERT INTO tbl_tr(location) VALUES('location')";
-	$sql_text_insert = "INSERT INTO tbl_tr(kva) VALUES('$kva')";
-	$sql_text_insert = "INSERT INTO tbl_tr(phase) VALUES('$phase')";
-	$sql_text_insert = "INSERT INTO tbl_tr(current-a-l) VALUES('$C_A_L')";
-	$sql_text_insert = "INSERT INTO tbl_tr(current-b-l) VALUES('$C_B_L')";
-	$sql_text_insert = "INSERT INTO tbl_tr(current-c-l) VALUES('C_C_L')";
-	$sql_text_insert = "INSERT INTO tbl_tr(current-a-r) VALUES('$C_A_R')";
-	$sql_text_insert = "INSERT INTO tbl_tr(current-b-r) VALUES('$C_B_R')";
-	$sql_text_insert = "INSERT INTO tbl_tr(current-c-r) VALUES('$C_C_R')";
-	$sql_text_insert = "INSERT INTO tbl_tr(voltage-an-l) VALUES('$V_AN_L')";
-	$sql_text_insert = "INSERT INTO tbl_tr(voltage-bn-l) VALUES('$V_BN_L')";
-	$sql_text_insert = "INSERT INTO tbl_tr(voltage-cn-l) VALUES('$V_CN_L')";
-	$sql_text_insert = "INSERT INTO tbl_tr(voltage-an-r) VALUES('$V_AN_R')";
-	$sql_text_insert = "INSERT INTO tbl_tr(voltage-bn-r) VALUES('$V_BN_R')";
-	$sql_text_insert = "INSERT INTO tbl_tr(voltage-cn-r) VALUES('$V_CN_R')";
-	
-	
+	$loc = $_POST["loc"];
+	$kva = $_POST["kva"];
+	$phase = $_POST["phase"];
+	$C_A_L = $_POST["C_A_L"];
+	$C_B_L = $_POST["C_B_L"];
+	$C_C_L = $_POST["C_C_L"];
+	$C_A_R = $_POST["C_A_R"];
+	$C_B_R = $_POST["C_B_R"];
+	$C_C_R = $_POST["C_C_R"];
+	$V_AN_L = $_POST["V_AN_L"];
+	$V_BN_L = $_POST["V_BN_L"];
+	$V_CN_L = $_POST["V_CN_L"];
+	$V_AN_R = $_POST["V_AN_R"];
+	$V_BN_R = $_POST["V_BN_R"];
+	$V_CN_R = $_POST["V_CN_R"];
+		
+	$sql_text_insert = "INSERT INTO tbl_tr(pea,loc,kva,phase,current-a-l,current-b-l,current-c-l,current-a-r,current-b-r,current-c-r,voltage-an-l,voltage-bn-l,voltage-cn-l,voltage-an-r,voltage-bn-r,voltage-cn-r) VALUES('$pea','$loc','$kva','$phase','$C_A_L','$C_B_L','$C_C_L','$C_A_R','$C_B_R','$C_C_R','$V_AN_L','$V_BN_L','$V_CN_L','$V_AN_R','$V_BN_R','$V_CN_R')";
+		
 	mysqli_query($conn,$sql_text_insert);
 	echo "Insert Complete.....";
 ?>
